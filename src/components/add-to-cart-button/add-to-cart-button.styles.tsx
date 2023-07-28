c2b55ea933c14e41e23e85add9a999a3bba4fe38
Edward Park <edwardpark123@gmail.com>
2023-07-28 13:50:24 -0700

    AddToCartButton with styling

---
+++ src/components/add-to-cart-button/add-to-cart-button.styles.tsx
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components';
+
+export const AddButton = styled.div`
+  display: flex;
+  width: 456px;
+  padding: 20px 80px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 10px;
+  background: var(--black, #201b21);
+  color: white;
+  font-family: Inter;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px;
+`;
---
+++ src/components/add-to-cart-button/add-to-cart-button.tsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import { AddButton } from "./add-to-cart-button.styles";
+
+const AddToCartButton = () => {
+  return (
+
+    <>
+    <AddButton>Add to Cart</AddButton>
+  </>
+    )
+}
+
+export default AddToCartButton;
