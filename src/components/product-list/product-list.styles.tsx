187687b944eb0b58cbb14396fd54c66538fee95d
Edward Park <edwardpark123@gmail.com>
2023-07-28 18:40:15 -0700

    ProductList component with styling

---
+++ src/components/product-list/product-list.styles.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ProductsContainer = styled.div`
+  width: 80%;
+  display: flex;
+`;
+
+export const ExploreText = styled.h1`
+  align-self: flex-start;
+  margin-left: 10%;
+  color: var(--black, #201b21);
+  font-family: DM Sans;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 48px; /* 133.333% */
+  letter-spacing: -1px;
+`;
--- src/components/product-list/product-list.tsx
+++ src/components/product-list/product-list.tsx
@@ -1,20 +1,26 @@
 import { useContext, FC } from 'react';
 import { ProductsContext } from '../../contexts/product.context';
 import ProductCard from '../product-card/product-card';
+import {
+  Container,
+  ProductsContainer,
+  ExploreText,
+} from './product-list.styles';
+// import Thumbnails from '../../../public/images/products/thumbnails/cosmic-unity-3.png';
 
 const ProductList: FC = () => {
   const { products } = useContext(ProductsContext);
 
   return (
     <>
-      <div>
-        Explore the latest drops
-        <div>
+      <Container>
+        <ExploreText>Explore the latest drops</ExploreText>
+        <ProductsContainer>
           {products.map((product) => (
             <ProductCard key={product['id']} product={product} />
           ))}
-        </div>
-      </div>
+        </ProductsContainer>
+      </Container>
     </>
   );
 };
