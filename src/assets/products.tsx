826158162f26a26d9a444f1034412ffbfd745bed
Edward Park <edwardpark123@gmail.com>
2023-07-31 18:59:55 -0700

    reorganized files

---
+++ src/assets/products.tsx
@@ -0,0 +1,38 @@
+import Adidas1 from './images/products/adidas/1.png';
+import Adidas2 from './images/products/adidas/2.png';
+import Adidas3 from './images/products/adidas/3.png';
+import Adidas4 from './images/products/adidas/4.png';
+
+import OffWhite1 from './images/products/Off-White/1.png';
+import OffWhite2 from './images/products/Off-White/2.png';
+import OffWhite3 from './images/products/Off-White/3.png';
+import OffWhite4 from './images/products/Off-White/4.png';
+
+import NikeCosmicUnity1 from './images/products/Nike/CosmicUnity/1.png';
+import NikeCosmicUnity2 from './images/products/Nike/CosmicUnity/2.png';
+import NikeCosmicUnity3 from './images/products/Nike/CosmicUnity/3.png';
+import NikeCosmicUnity4 from './images/products/Nike/CosmicUnity/4.png';
+
+import NikeGammaForce1 from './images/products/Nike/GammaForce/1.png';
+import NikeGammaForce2 from './images/products/Nike/GammaForce/2.png';
+import NikeGammaForce3 from './images/products/Nike/GammaForce/3.png';
+import NikeGammaForce4 from './images/products/Nike/GammaForce/4.png';
+
+export const PhotoUrlList = [
+  Adidas1,
+  Adidas2,
+  Adidas3,
+  Adidas4,
+  OffWhite1,
+  OffWhite2,
+  OffWhite3,
+  OffWhite4,
+  NikeCosmicUnity1,
+  NikeCosmicUnity2,
+  NikeCosmicUnity3,
+  NikeCosmicUnity4,
+  NikeGammaForce1,
+  NikeGammaForce2,
+  NikeGammaForce3,
+  NikeGammaForce4,
+];
